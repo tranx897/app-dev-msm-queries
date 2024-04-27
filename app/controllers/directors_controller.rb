2dd10b6c47066643d2a1094c942daad8a76a4361
tranx897 <63356332+tranx897@users.noreply.github.com>
2024-04-27 23:01:43 +0000

    add filmography

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -2,4 +2,13 @@ class DirectorsController < ApplicationController
   def index
     render({ :template => "director_templates/list" })
   end
+
+  def show
+    director_id = params["director_id"]
+    matching_records = Director.where({:id => director_id})
+    @director = matching_records[0]
+    @matching_movies = Movie.where({:director_id => director_id})
+
+    render({ :template => "director_templates/details"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
   get("/directors", {:controller => "directors", :action => "index"})
+  get("/directors/:director_id", {:controller => "directors", :action => "show"})
+  get("/movies", {:controller => "movies", :action => "index"})
 end
